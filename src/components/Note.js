78972ed0275a770ac231f44f83ff575f076f1cb2
OMEGA <omega5505.coding@gmail.com>
2021-12-12 16:18:58 +0530

    feat - Added tooltip

--- src/components/Note.js
+++ src/components/Note.js
@@ -8,6 +8,7 @@ const Note = ({ id, text, date, deleteNote, category }) => {
         <small>{category}</small>
 
         {/* Delete icon */}
+        <div className="delete-icon">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -20,10 +21,10 @@ const Note = ({ id, text, date, deleteNote, category }) => {
           <path d="M5 20a2 2 0 002 2h10a2 2 0 002-2V8h2V6h-4V4a2 2 0 00-2-2H9a2 2 0 00-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path>
           <path d="M9 10h2v8H9zm4 0h2v8h-2z"></path>
 
-          {/* #delete-icon
-          onClick={() => deleteNote(id)} */}
           <title>Delete note</title>
         </svg>
+          <span className="tooltip">Delete</span>
+        </div>
       </div>
     </div>
   )
